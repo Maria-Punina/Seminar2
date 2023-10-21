a5f21468cfd83314b5bd5b6b33b482f3a52b2184
Maria Punina <puninama@mail.ru>
2023-10-22 04:51:48 +0500

    task1

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(99 < number && number < 1000)
+{
+   int lastnumber = number /10 % 10;
+  
+     Console.WriteLine(lastnumber);
+}
+else
+{
+   Console.WriteLine("Введитие число меньше 1000, но больше 99");
+}
